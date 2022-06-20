17a99abe9a01186d6a6bb523cd33234922bab823
gleb_danichev <g.danichev@pointpay.io>
2022-06-20 15:58:31 +0300

    XCH-1705 Add log for initCurrentCandles

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -145,7 +145,6 @@ func initCurrentCandles(ctx context.Context, service *candle.Service, marketsMap
 		WithField("elapsed", time.Since(started).String()).
 		WithField("keys", keys).
 		Infof("initiated %candles from MongoDb")
-	os.Exit(0)
 	return candles
 }
 
