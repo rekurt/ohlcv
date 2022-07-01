cb9361f9368ff271abce16f8bf518d382cf9b28f
gleb_danichev <g.danichev@pointpay.io>
2022-07-01 14:41:04 +0300

    XCH-1831 Add i++

--- domain/microbatching.go
+++ domain/microbatching.go
@@ -46,18 +46,21 @@ func Microbatching(ctx context.Context, chartStream chan *Chart, maxBatchSize in
 func mergeSameChart(batch []*Chart) []*Chart {
 	var ans []*Chart
 
-	for i := range batch {
-		if i+1 < len(batch) && batch[i].Symbol == batch[i+1].Symbol && batch[i].Resolution == batch[i+1].Resolution {
+	for i := 0; i < len(batch); i++ {
+		if i+1 < len(batch) &&
+			batch[i].Symbol == batch[i+1].Symbol &&
+			batch[i].Resolution == batch[i+1].Resolution {
 			ans = append(ans, &Chart{
-				Symbol:     "",
-				Resolution: "",
-				O:          nil,
-				H:          nil,
-				L:          nil,
-				C:          nil,
-				V:          nil,
-				T:          nil,
+				Symbol:     batch[i].Symbol,
+				Resolution: batch[i].Resolution,
+				O:          append(batch[i].O, batch[i+1].O...),
+				H:          append(batch[i].H, batch[i+1].H...),
+				L:          append(batch[i].L, batch[i+1].L...),
+				C:          append(batch[i].C, batch[i+1].C...),
+				V:          append(batch[i].V, batch[i+1].V...),
+				T:          append(batch[i].T, batch[i+1].T...),
 			})
+			i++
 		} else {
 			ans = append(ans, batch[i])
 		}
