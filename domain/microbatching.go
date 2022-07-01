97c35bfb22e40ce4fb6cd1b77d075100ca0a7b2a
gleb_danichev <g.danichev@pointpay.io>
2022-07-01 11:34:49 +0300

    Add mergeSameChart function

--- domain/microbatching.go
+++ domain/microbatching.go
@@ -42,3 +42,26 @@ func Microbatching(ctx context.Context, chartStream chan *Chart, maxBatchSize in
 	}()
 	return batchStream
 }
+
+func mergeSameChart(batch []*Chart) []*Chart {
+	var ans []*Chart
+
+	for i := range batch {
+		if i+1 < len(batch) && batch[i].Symbol == batch[i+1].Symbol && batch[i].Resolution == batch[i+1].Resolution {
+			ans = append(ans, &Chart{
+				Symbol:     "",
+				Resolution: "",
+				O:          nil,
+				H:          nil,
+				L:          nil,
+				C:          nil,
+				V:          nil,
+				T:          nil,
+			})
+		} else {
+			ans = append(ans, batch[i])
+		}
+	}
+
+	return ans
+}
