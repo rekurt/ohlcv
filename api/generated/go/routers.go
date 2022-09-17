b5b7c07213edd73f5e73fabf6a8a10ce0498186d
nikita aldaev <n.aldaev@pointpay.io>
2022-09-17 18:13:12 +0700

    CORS

--- api/generated/go/routers.go
+++ api/generated/go/routers.go
@@ -65,9 +65,8 @@ func EncodeJSONResponse(i interface{}, status *int, w http.ResponseWriter, r *ht
 
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, sentry-trace")
+	w.Header().Set("Access-Control-Allow-Headers", "*")
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
-	w.Header().Set("Access-Control-Max-Age", "86400")
 
 	if status != nil {
 		w.WriteHeader(*status)
