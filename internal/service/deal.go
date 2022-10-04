65421474ab7fca7a6c40fe10070ff5d2752f5aee
olegmaslovsky <o.maslovsky@pointpay.io>
2022-10-04 11:44:24 +0300

    Revert "Add extra logs"
    This reverts commit 301547e34f6cbcd3d5eb8dbaed949d5ab0da921f.

--- internal/service/deal.go
+++ internal/service/deal.go
@@ -65,7 +65,6 @@ func (s *Deal) SaveDeal(ctx context.Context, dealMessage *matcher.Deal) (*model.
 	}
 	select {
 	case s.eventChanel <- deal:
-		logger.FromContext(ctx).Infof("send deal to consumer %v", *deal)
 	default:
 		logger.FromContext(ctx).Errorf("deal channel overloaded")
 	}
@@ -140,7 +139,6 @@ func (s *Deal) RunConsuming(ctx context.Context, consumer pubsub.Subscriber, top
 							"unmarshal error with protobuf deals msg",
 						)
 					}
-					logger.FromContext(ctx).Infof("got deal %v", dealMessage.Market)
 					err := currentCandles.AddDeal(dealMessage)
 					if err != nil {
 						logger.FromContext(ctx).
@@ -150,7 +148,6 @@ func (s *Deal) RunConsuming(ctx context.Context, consumer pubsub.Subscriber, top
 					if deal, err := s.SaveDeal(ctx, dealMessage); err != nil {
 						return errors.Wrapf(err, "while saving deal %v into DB", deal)
 					}
-					logger.FromContext(ctx).Infof("saved deal %v", dealMessage.Market)
 					return nil
 				},
 			)
